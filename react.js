baf575d4507f18eb7ba905d21605f40613288a95
Mohamad Yusry Amir Bin Ismail <ysryismail@gmail.com>
2025-09-04 02:24:07 +0800

    Create react.js

---
+++ react.js
@@ -0,0 +1,10 @@
+<iframe
+  src="https://aura.build/share/5PPO3HV"
+  style={{
+    width: "100%",
+    height: "500px",
+    border: "none",
+    borderRadius: "8px",
+  }}
+  title="Horizontal Scroll Panel Interface"
+/>
